7e0fdf7dcbfc822299e6660d992e04941f2df980
HAIZAKURA <hhbilly99@gmail.com>
2021-02-18 16:07:54 +0800

    Delete SmmsMapper.java

--- src/main/java/run/nya/petbbs/mapper/SmmsMapper.java
+++
@@ -1,7 +0,0 @@
-package run.nya.petbbs.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import run.nya.petbbs.entity.SmmsConfig;
-
-public interface SmmsMapper extends BaseMapper<SmmsConfig> {
-}
