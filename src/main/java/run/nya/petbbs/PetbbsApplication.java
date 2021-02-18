e9574b8028957a3d086f998f6b816a53dd713dbb
HAIZAKURA <hhbilly99@gmail.com>
2021-02-18 16:08:21 +0800

    Add Annotation

--- src/main/java/run/nya/petbbs/PetbbsApplication.java
+++ src/main/java/run/nya/petbbs/PetbbsApplication.java
@@ -1,8 +1,12 @@
 package run.nya.petbbs;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
+@MapperScan("run.nya.petbbs.mapper")
 @SpringBootApplication
 public class PetbbsApplication {
 
