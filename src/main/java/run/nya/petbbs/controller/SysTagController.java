99f79365337d89efdcc016ed4aadd812b702e3a9
HAIZAKURA <hhbilly99@gmail.com>
2021-04-20 22:32:10 +0800

    Update SysTagController.java

--- src/main/java/run/nya/petbbs/controller/SysTagController.java
+++ src/main/java/run/nya/petbbs/controller/SysTagController.java
@@ -99,7 +99,7 @@ public class SysTagController extends BaseController {
             return ApiResult.failed("标签不存在");
         }
         iSysTagService.removeById(id);
-        return ApiResult.failed("删除成功");
+        return ApiResult.success("删除成功");
     }
 
 }
