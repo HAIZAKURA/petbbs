f7c6da23fb9e77e44006194f43c778f2ad8b550c
HAIZAKURA <hhbilly99@gmail.com>
2021-06-01 20:18:48 +0800

    Update JwtTokenUtil.java

--- src/main/java/run/nya/petbbs/config/security/util/JwtTokenUtil.java
+++ src/main/java/run/nya/petbbs/config/security/util/JwtTokenUtil.java
@@ -21,7 +21,7 @@ import java.util.Map;
  * header的格式（算法、token的类型）：
  * {"alg": "HS512","typ": "JWT"}
  * payload的格式（用户名、创建时间、生成时间）：
- * {"sub":"wang","created":1489079981393,"exp":1489684781}
+ * {"sub":"jiang","created":1489079981393,"exp":1489684781}
  * signature的生成算法：
  * HMACSHA512(base64UrlEncode(header) + "." +base64UrlEncode(payload),secret)
  *
